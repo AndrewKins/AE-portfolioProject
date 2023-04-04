e5c36a4e619fd422859e21854c45229b24caf830
AndrewKins <andrewm.enoe@gmail.com>
2023-04-04 11:18:26 -0400

    update

--- portfolio/components/Bio.tsx
+++ portfolio/components/Bio.tsx
@@ -1,12 +1,12 @@
 
 const Bio = () => {
     return (
-        <div className = "container mx-small" >
-            <h1 className="text-2xl text-white font-semibold px-9">My Projects</h1>
+        <div className = "container bg-white mx-small" >
+            {/* <h1 className="text-2xl text-white font-semibold px-9">My Projects</h1>
             <div className="bg-white rounded-lg shadow-md p-6 max-w-sm">
             <h1 className="text-xl text-black font-bold mb-2">Card Title</h1>
             <p className="text-gray-700">This is the content of my card.</p>
-        </div>
+        </div> */}
         </div>
     )
 }
--- portfolio/components/Header.tsx
+++ portfolio/components/Header.tsx
@@ -2,20 +2,13 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="bg-gray-900 text-white py-6">
-      <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between">
-        <div className="flex items-center">
-            <a className="text-2xl font-bold">My Logo</a>
-        </div>
-        <ul className="flex space-x-4">
-          <li>
-              <a className="hover:text-gray-400">About</a>
-          </li>
-          <li>
-              <a className="hover:text-gray-400">Contact</a>
-          </li>
-        </ul>
-      </nav>
+    <header className="text-white ">
+      <nav className="flex justify-center space-x-4">
+  <a href="/dashboard" className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Home</a>
+  <a href="/Bio" className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Bio</a>
+  <a href="/Projects" className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Projects</a>
+  <a href="/Contact" className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Contact</a>
+</nav>
     </header>
   )
 }
