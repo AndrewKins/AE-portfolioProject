a09fb6e97db3cf385afc99c27a0049ec1b5f2cb3
AndrewKins <andrewm.enoe@gmail.com>
2023-04-04 10:00:44 -0400

    update

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -3,13 +3,14 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@component/styles/Home.module.css'
 import Header from '@component/components/Header'
-
+import Bio from '@component/components/Bio'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
       <div>
           <Header/>
+          <Bio/>
       </div>
   )
 }
